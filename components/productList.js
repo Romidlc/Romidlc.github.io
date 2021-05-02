438ace649bb167a857cc5bcb2d72b88f644410f3
rominadlc <romidlc3@gmail.com>
2021-05-02 12:55:56 -0300

    prefetched products on build time

--- components/productList.js
+++ components/productList.js
@@ -4,21 +4,22 @@ import ProductCardDetail from './productCardDetail';
 import { UserContext } from '../context/userContext';
 
 const ProductList = ({ prefetchedProducts }) => {
-   const [products, setProducts] = useState([]); 
+    console.log("pretefetched", prefetchedProducts)
+   const [products, setProducts] = useState(prefetchedProducts); 
    const [currentPoints, setCurrentPoints] = useState(0); 
    const [redeemProducts, setRedeemProducts] = useState([]); 
    const {currentUser} = useContext(UserContext)
 
-   const getProducts = async () => {
-    const fetchedProducts = await dataProvider('CUSTOM_GET', {
-        url: "products"
-    })
-    setProducts([...fetchedProducts])
-   } // get products from api
+//    const getProducts = async () => {
+//     const fetchedProducts = await dataProvider('CUSTOM_GET', {
+//         url: "products"
+//     })
+//     setProducts([...fetchedProducts])
+//    } // get products from api
 
-   useEffect(() => {
-     getProducts()  
-   }, [])
+//    useEffect(() => {
+//      getProducts()  
+//    }, [])
 
    useEffect(() => {
        setCurrentPoints(currentUser?.points)
@@ -28,13 +29,14 @@ const ProductList = ({ prefetchedProducts }) => {
    return(
        <>
        <div>
-           <p>{`your points: ${currentPoints}`}</p>
-           {redeemProducts?.map(product => 
-           <div style={{display: "flex", justifyContent: "space-around"}}>
+         <p>{`your points: ${currentPoints}`}</p>
+         {redeemProducts?.map((product, index) => 
+           <div style={{display: "flex", justifyContent: "space-around"}} key={index}>
                <p>{product?.name}</p>
                <p>{product?.category}</p>
                <p>{product?.cost}</p>
-           </div>)}
+           </div>
+         )}
         </div>
        <div className="productListContainer">
        {products?.map(product => 
@@ -46,12 +48,12 @@ const ProductList = ({ prefetchedProducts }) => {
 }
 
 export const getStaticProps = async () => {
-    const prefetechedProducts = await dataProvider('CUSTOM_GET', {
+    const prefetchedProducts = await dataProvider('CUSTOM_GET', {
         url: "products" // this endpoint should has a limit and skip params and only get por example the first 20 products. this will be a better experience for the user.
     });
     return {
         props: {
-          prefetechedProducts,
+          prefetchedProducts,
         },
         revalidate: 20
     }
